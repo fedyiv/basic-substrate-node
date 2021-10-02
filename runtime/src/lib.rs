a33a08a4b49ac3f8e5f47c473d36a5bd904350cb
fedyiv <ivan.a.fedyanin@gmail.com>
2021-10-02 21:21:43 +0300

    Forkless Upgrade using Sudo pallete - https://substrate.dev/docs/en/tutorials/forkless-upgrade/sudo-upgrade

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -98,7 +98,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	//   `spec_version`, and `authoring_version` are the same between Wasm and native.
 	// This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
 	//   the compatible custom types.
-	spec_version: 100,
+	spec_version: 101,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
@@ -284,6 +284,24 @@ impl pallet_nicks::Config for Runtime {
     type Event = Event;
 }
 
+// Define the types required by the Scheduler pallet.
+parameter_types! {
+  pub MaximumSchedulerWeight: Weight = 10_000_000;
+  pub const MaxScheduledPerBlock: u32 = 50;
+}
+
+/// Configure the runtime's implementation of the Scheduler pallet.
+impl pallet_scheduler::Config for Runtime {
+  type Event = Event;
+  type Origin = Origin;
+  type PalletsOrigin = OriginCaller;
+  type Call = Call;
+  type MaximumWeight = MaximumSchedulerWeight;
+  type ScheduleOrigin = frame_system::EnsureRoot<AccountId>;
+  type MaxScheduledPerBlock = MaxScheduledPerBlock;
+  type WeightInfo = ();
+}
+
 parameter_types! {
 	pub const TransactionByteFee: Balance = 1;
 }
@@ -319,6 +337,7 @@ construct_runtime!(
 		Grandpa: pallet_grandpa::{Pallet, Call, Storage, Config, Event},
 		Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
 		Nicks: pallet_nicks::{Pallet, Call, Storage, Event<T>},
+		Scheduler: pallet_scheduler::{Pallet, Call, Storage, Event<T>},
 		TransactionPayment: pallet_transaction_payment::{Pallet, Storage},
 		Sudo: pallet_sudo::{Pallet, Call, Config<T>, Storage, Event<T>},
 		// Include the custom logic from the pallet-template in the runtime.
